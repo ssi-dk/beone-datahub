27097f1bc7725fcf86c06ae6bef92e3caa2429d7
Finn Gruwier Larsen <figl@ssi.dk>
2022-07-19 15:15:16 +0200

    Use a template for rendering.

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("This is the home view.")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    # return render(request, 'polls/index.html', context)
+    return render(request, 'main/base.html')
