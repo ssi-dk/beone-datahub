7e7b83aa22554b0ed5ec23fc9b3a872e58079b5a
Finn Gruwier Larsen <figl@ssi.dk>
2022-07-19 14:29:54 +0200

    Add ALL_SPECIES dict

--- beone_web_app/settings.py
+++ beone_web_app/settings.py
@@ -127,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Own additions
+ALL_SPECIES = {
+    'campy': 'Campylobacter jejuni',
+    'coli': 'Escherichia coli',
+    'listeria': 'Listeria monocytogenes',
+    'salmonella': 'Salmonella enterica',
+}
