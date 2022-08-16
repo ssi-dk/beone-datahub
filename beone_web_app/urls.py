bb11f0758926ea10ceb59c369219920552443631
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-16 19:17:22 +0200

    Reintroduce sample_list view.

--- beone_web_app/urls.py
+++ beone_web_app/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', views.redirect_root),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html', next_page='/sample_list'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login'), name='logout'),
-    path('sample_list/', views.DataSetView.as_view(), name='sample_list'),
+    path('sample_list/', views.sample_list, name='sample_list'),
     path('sample_list/<int:dataset_key>/', views.DataSetView.as_view(), name='view_dataset'),
     path('sample_list/<int:dataset_key>/edit', views.DataSetView.as_view(edit=True), name='edit_dataset'),
     path('datasets/', views.dataset_list, name='datasets'),
--- main/views.py
+++ main/views.py
@@ -32,6 +32,32 @@ def redirect_root(request):
         return HttpResponseRedirect('/login/')
     
 
+@login_required
+def sample_list(request, dataset_key:int=None):
+    user_profile = get_context(request)
+    if dataset_key:
+        dataset = DataSet.objects.get(pk=dataset_key)
+        species = dataset.species
+    else:
+        dataset = None
+        species = None
+    species_name = get_species_name(species)
+    samples = list(api.get_samples(species_name=species_name))
+    print(f"***Dataset: {dataset}")
+    for sample in samples:
+        sample['id'] = str(sample['_id'])
+        if dataset:
+            sample['in_dataset'] = sample['id'] in dataset.mongo_ids
+        else:
+            sample['in_dataset'] = False
+    return render(request, 'main/sample_list.html',{
+        'user_profile': user_profile,
+        'species_name': species_name,
+        'samples': samples,
+        'dataset': dataset
+        })
+
+
 class DataSetView(View):
     edit:bool=False
 
