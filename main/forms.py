e7b2e3044d56044e9bf6b939bcd524b887b82b0d
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-17 17:25:19 +0200

    Add form labels.

--- main/forms.py
+++ main/forms.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 class NewDatasetForm(forms.Form):
     species = forms.ChoiceField(label='Select species:', choices=settings.ALL_SPECIES)
-    name = forms.CharField(max_length=40)
-    description = forms.CharField(max_length=200)
+    name = forms.CharField(max_length=40, label='Unique name')
+    description = forms.CharField(max_length=200, required=False, label='Optional description')
