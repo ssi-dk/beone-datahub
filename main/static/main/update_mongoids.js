81e0e68d2905e3568ae4f58df858857293255927
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-19 14:39:48 +0200

    Make the script do something meaningful.

--- main/static/main/update_mongoids.js
+++ main/static/main/update_mongoids.js
@@ -3,3 +3,13 @@ let jsonToSend = {
     "datasetOwner": document.getElementById("dataset_owner").innerText,
 }
 console.log(jsonToSend)
+
+
+document.getElementById("sample_list").addEventListener("click", addOrRemove);
+
+function addOrRemove(event) {
+    if (event.target.type === 'checkbox') {
+        console.log(event.target.id);
+        console.log(event.target.checked);
+    }
+}
