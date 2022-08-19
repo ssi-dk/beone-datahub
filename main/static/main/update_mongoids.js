e49abfcafdcd21bd282ccdc13134677bd7cae532
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-19 13:54:48 +0200

    Improve JavaScript.

--- main/static/main/update_mongoids.js
+++ main/static/main/update_mongoids.js
@@ -1,3 +1,4 @@
 console.log('Dataset:')
-console.log(document.getElementById('dataset_name'))
-console.log(document.getElementById('dataset_owner'))
+console.log(document.getElementById('dataset_name').innerText)
+console.log('Username:')
+console.log(document.getElementById('dataset_owner').innerText)
