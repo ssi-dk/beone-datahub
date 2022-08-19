c0a0ef2f0e65ad947d61fb088474b3314c7d3b44
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-19 14:04:05 +0200

    Further improvement.

--- main/static/main/update_mongoids.js
+++ main/static/main/update_mongoids.js
@@ -1,4 +1,5 @@
-console.log('Dataset:')
-console.log(document.getElementById('dataset_name').innerText)
-console.log('Username:')
-console.log(document.getElementById('dataset_owner').innerText)
+let jsonToSend = {
+    "datasetName": document.getElementById("dataset_name").innerText,
+    "datasetOwner": document.getElementById("dataset_owner").innerText,
+}
+console.log(jsonToSend)
