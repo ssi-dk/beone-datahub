c7d5a39d89735c6b258eb63cd4242c029a2f20d7
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-19 14:55:55 +0200

    Make the script even better.

--- main/static/main/update_mongoids.js
+++ main/static/main/update_mongoids.js
@@ -1,19 +1,17 @@
-let jsonToSend = {
-    "datasetName": document.getElementById("dataset_name").innerText,
-    "datasetOwner": document.getElementById("dataset_owner").innerText,
-}
-console.log(jsonToSend)
-
-
 document.getElementById('sample_list').addEventListener('click', addOrRemove);
 
 function addOrRemove(event) {
     if (event.target.type === 'checkbox') {
-        console.log(event.target.id);
-        if (event.target.checked) {
-            console.log('Add')
-        } else {
-            console.log('Remove')
+        let jsonToSend = {
+            "datasetName": document.getElementById("dataset_name").innerText,
+            "datasetOwner": document.getElementById("dataset_owner").innerText,
         }
+        jsonToSend["mongoId"] = event.target.id
+        if (event.target.checked) {
+            jsonToSend["action"] = 'Add'
+        } else {
+            jsonToSend["action"] = 'Remove'
+        }
+        console.log(jsonToSend)
     }
 }
