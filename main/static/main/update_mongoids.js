ad69b71dce847a4f62f7ca9c5c3e8d5b1644d03c
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-22 14:18:26 +0200

    POST now working.

--- main/static/main/update_mongoids.js
+++ main/static/main/update_mongoids.js
@@ -1,3 +1,23 @@
+
+// Todo: check this with current Django documentation.
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+
 document.getElementById('sample_list').addEventListener('click', addOrRemove);
 
 function addOrRemove(event) {
@@ -15,9 +35,12 @@ function addOrRemove(event) {
         }
         let url = window.location.origin + '/datasets/add_remove_sample/'
         fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
             headers:{
                 'Accept': 'application/json',
                 'X-Requested-With': 'XMLHttpRequest',
+                'X-CSRFToken': csrftoken,
             },
         })
         .then(response => {
