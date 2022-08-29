0134afe79567c08f2113b59702a0739f4876f567
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-29 15:31:21 +0200

    Make command option work.

--- main/management/commands/addids.py
+++ main/management/commands/addids.py
@@ -1,3 +1,4 @@
+from lib2to3.pytree import Base
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
@@ -10,7 +11,14 @@ class Command(BaseCommand):
         parser.add_argument('owning_org', type=str)
 
     def handle(self, *args, **options):
+        print(options['owning_org'])
+
         connection = pymongo.MongoClient(settings.MONGO_CONNECTION)
         db = connection.get_database()
-        samples = db.samples.find({})
+        number = db.samples.count_documents({})
+        self.stdout.write(f"{str(number)} samples found.")
+
+        new_values = { "$set": { "owning_org": "SSI" } }
+        # db.samples.update_many({}, )
+
         self.stdout.write(self.style.SUCCESS('Success!'))
