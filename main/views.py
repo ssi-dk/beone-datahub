6f8dde34b9a54b85484e4a720fcda34dc816dba3
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-31 10:32:40 +0200

    Don't convert Mongo cursor to list here.

--- main/views.py
+++ main/views.py
@@ -75,7 +75,7 @@ def dataset_list(request):
 def view_dataset(request, dataset_key:int):
     dataset = DataSet.objects.get(pk=dataset_key)
     species_name = get_species_name(dataset.species)
-    samples = list(api.get_samples_from_keys(dataset.mongo_keys))
+    samples = api.get_samples_from_keys(dataset.mongo_keys)
 
     return render(request, 'main/sample_list.html',{
         'species_name': species_name,
