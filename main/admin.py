c5ffe313a1c601331a60ab4bfb6fc7ad01e01f4d
Finn Gruwier Larsen <figl@ssi.dk>
2022-09-01 14:19:20 +0200

    Register RTJob with Admin.

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import DataSet
+from main.models import DataSet, RTJob
 
 admin.site.register(DataSet)
+admin.site.register(RTJob)
