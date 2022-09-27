6719c8720b11c5d35982791f1077cc57adafd684
Finn Gruwier Larsen <figl@ssi.dk>
2022-09-27 12:56:59 +0200

    Start time, end time, elapsed time.

--- reportree/rest_interface/main.py
+++ reportree/rest_interface/main.py
@@ -1,4 +1,5 @@
 import subprocess
+from datetime import datetime
 
 from fastapi import FastAPI
 
@@ -11,6 +12,7 @@ async def root():
 
 @app.post("/reportree/start_job/{job_number}")
 async def start_job(job_number: int):
+    start_time = datetime.now()
     command = [
         'python',
         '/app/ReporTree/reportree.py',
@@ -24,8 +26,12 @@ async def start_job(job_number: int):
     out, err = p.communicate()
     print('Subprocess error: ' + str(err))
     print('Subprocess stdout: ' + str(out.decode()))   
-
+    end_time = datetime.now()
+    elapsed_time = end_time - start_time
     return {
         "job: number": job_number,
+        "start_time": start_time.isoformat(),
+        "end_time": end_time.isoformat(),
+        "elapsed_time": elapsed_time,
         "err": err
         }
