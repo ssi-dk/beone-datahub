b43eba83f39dac62248fffed77a102e76ab63cb4
Finn Gruwier Larsen <figl@ssi.dk>
2022-09-27 08:47:39 +0200

    Add start_job endpoint.

--- reportree/rest_interface/main.py
+++ reportree/rest_interface/main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/reportree/start_job/{job_number}")
+async def start_job(job_number: int):
+    return {
+        "job: number": job_number,
+        "status": "OK"
+        }
