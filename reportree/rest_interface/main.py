e883954a360d44123a2e81d415b48d4ca0bb3d36
Finn Gruwier Larsen <figl@ssi.dk>
2022-09-27 11:56:07 +0200

    Run a real command with RT.

--- reportree/rest_interface/main.py
+++ reportree/rest_interface/main.py
@@ -11,14 +11,20 @@ async def root():
 
 @app.post("/reportree/start_job/{job_number}")
 async def start_job(job_number: int):
-
-    p = subprocess.Popen(['python', '/app/ReporTree/reportree.py', '--help'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    command = [
+        'python',
+        '/app/ReporTree/reportree.py',
+        '-a',
+        f'/mnt/rt_runs/{job_number}/allele_profiles.tsv',
+        '--analysis',
+        'HC'
+        ]
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     out, err = p.communicate()
     print('Subprocess error: ' + str(err))
     print('Subprocess stdout: ' + str(out.decode()))   
 
     return {
         "job: number": job_number,
-        "out": out,
         "err": err
         }
