495bc0ddf9231fa8fb8fca9e72541c37b7e022d2
Finn Gruwier Larsen <figl@ssi.dk>
2022-10-21 12:40:04 +0200

    Add class CLusterAdmin.

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from main.models import DataSet, RTJob, Cluster
+
+class ClusterAdmin(admin.ModelAdmin):
+    list_display = ('rt_job', 'partition', 'cluster_no')
 
 admin.site.register(DataSet)
 admin.site.register(RTJob)
-admin.site.register(Cluster)
+admin.site.register(Cluster, ClusterAdmin)
