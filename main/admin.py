c765192b917d6e11154b22b072b182ac53041af7
Finn Gruwier Larsen <figl@ssi.dk>
2022-10-27 10:04:23 +0200

    Pare partitions

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from main.models import DataSet, RTJob, Cluster
-
-class ClusterAdmin(admin.ModelAdmin):
-    pass
-    # list_display = ('rt_job', 'partition', 'cluster_name')
+from main.models import DataSet, RTJob, Partition, Cluster
 
 admin.site.register(DataSet)
 admin.site.register(RTJob)
+
+class PartitionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rt_job')
+
+admin.site.register(Partition, PartitionAdmin)
+
+class ClusterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'partition')
+
 admin.site.register(Cluster, ClusterAdmin)
+
--- main/models.py
+++ main/models.py
@@ -205,29 +205,37 @@ def parse_rt_output(rt_job: RTJob):
       rt_job.log = f.read()
    with open(rt_job.get_newick_path(), 'r') as f:
       rt_job.newick = f.read()
+   
+   # Parse partitions report and create Partition objects in db
    with open(rt_job.get_partitions_path(), 'r') as f:
-      rt_job.partitions = f.read()
+      lines = f.readlines()
+      partition_names = lines[0].strip().split('\t')
+      partition_names = partition_names[1:]
+      print(partition_names)
+      for name in partition_names:
+         partition = Partition(rt_job=rt_job, name=name)
+         partition.save()
    
    # Parse cluster report and create Cluster objects in db
-   with open(rt_job.get_cluster_path(), 'r') as f:
-      cluster_lines = f.readlines()
-      cluster_lines = cluster_lines[1:]  # Skip header line.
-   for cluster_line in cluster_lines:
-      cluster_line = cluster_line.strip()
-      pa, cl, _len, sam = cluster_line.split('\t')  # partition, cluster, cluster length, samples
-      cluster = Cluster(partition=pa, cluster_name=cl)
-      sample_str_list = sam.split(',')
-      # Transform sample_str_list to list of dicts
-      sample_list = list()
-      for sample_str in sample_str_list:
-         elements = sample_str.split('.')
-         assert len(elements) == 2
-         sample_dict = {'org': elements[0], 'name': elements[1]}
-         sample_list.append(sample_dict)
-      cluster.samples = sample_list
-      cluster.rt_job = rt_job
-      # TODO Use this field or delete it from model
-      cluster.allelic_distance = 1
-      cluster.save()
-   rt_job.set_status('ALL_DONE')
-   rt_job.save()
+   # with open(rt_job.get_cluster_path(), 'r') as f:
+   #    cluster_lines = f.readlines()
+   #    cluster_lines = cluster_lines[1:]  # Skip header line.
+   # for cluster_line in cluster_lines:
+   #    cluster_line = cluster_line.strip()
+   #    pa, cl, _len, sam = cluster_line.split('\t')  # partition, cluster, cluster length, samples
+   #    cluster = Cluster(partition=pa, cluster_name=cl)
+   #    sample_str_list = sam.split(',')
+   #    # Transform sample_str_list to list of dicts
+   #    sample_list = list()
+   #    for sample_str in sample_str_list:
+   #       elements = sample_str.split('.')
+   #       assert len(elements) == 2
+   #       sample_dict = {'org': elements[0], 'name': elements[1]}
+   #       sample_list.append(sample_dict)
+   #    cluster.samples = sample_list
+   #    cluster.rt_job = rt_job
+   #    # TODO Use this field or delete it from model
+   #    cluster.allelic_distance = 1
+   #    cluster.save()
+   # rt_job.set_status('ALL_DONE')
+   # rt_job.save()
