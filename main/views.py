631ff2c2841cd0405bcff2add2ed8c961c421f7a
Finn Gruwier Larsen <figl@ssi.dk>
2022-10-27 10:55:59 +0200

    Get partitions for job.

--- main/views.py
+++ main/views.py
@@ -12,7 +12,7 @@ from django.db import IntegrityError
 from django.contrib.auth.models import User
 
 from main.mongo.samples_api import API
-from main.models import Cluster, DataSet, RTJob, parse_rt_output
+from main.models import DataSet, RTJob, Partition,  Cluster, parse_rt_output
 from main.forms import NewDatasetForm, DeleteDatasetForm, DashboardLauncherForm
 
 api = API(settings.MONGO_CONNECTION, settings.MONGO_FIELD_MAPPING)
@@ -255,11 +255,8 @@ def get_rt_data(request, rt_job_key: str):
     rt_job = RTJob.objects.get(pk=rt_job_key)
     return JsonResponse({'newick': rt_job.newick, 'sample_ids': rt_job.dataset.mongo_keys})
 
-# @login_required
-# def get_rt_clusters(request, rt_job_key: str):
-#     rt_clusters = Cluster.objects.filter(rt_job=rt_job_key)
-#     cluster_dict = dict()
-#     for cluster in rt_clusters:
-#         name = cluster.cluster_name
-#         cluster_dict[name] = 'hej'
-#     return JsonResponse(cluster_dict)
+@login_required
+def get_partitions_for_job(request, rt_job_key: str):
+    rt_partitions = Partition.objects.filter(rt_job=rt_job_key)
+    partition_list = [ p.name for p in rt_partitions ]
+    return JsonResponse(partition_list, safe=False)
