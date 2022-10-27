4428c9692fd1ac827fda06c8881b0f05d6c63a9f
Finn Gruwier Larsen <figl@ssi.dk>
2022-10-27 09:33:41 +0200

    Change some naming.

--- main/models.py
+++ main/models.py
@@ -215,8 +215,8 @@ def parse_rt_output(rt_job: RTJob):
       cluster_lines = cluster_lines[1:]  # Skip header line.
    for cluster_line in cluster_lines:
       cluster_line = cluster_line.strip()
-      pa, cn, clen, sam = cluster_line.split('\t')  # partition, cluster name, cluster length, samples
-      cluster = Cluster(partition=pa, cluster_name=cn)
+      pa, cl, _len, sam = cluster_line.split('\t')  # partition, cluster, cluster length, samples
+      cluster = Cluster(partition=pa, cluster_name=cl)
       sample_str_list = sam.split(',')
       # Transform sample_str_list to list of dicts
       sample_list = list()
