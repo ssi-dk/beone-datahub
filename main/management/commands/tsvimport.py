fb077e30b66191be6aa60cae5bd0883c25c47ec4
Finn Gruwier Larsen <figl@ssi.dk>
2022-11-10 10:51:19 +0100

    Get headers from files.

--- main/management/commands/tsvimport.py
+++ main/management/commands/tsvimport.py
@@ -38,6 +38,28 @@ class Command(BaseCommand):
         number = db.samples.count_documents({})
         self.stdout.write(f'MongoDB currently contains {str(number)} samples (before import).')
 
+        
+        with open(a_path) as a_file:
+            with open(m_path) as m_file:
+                a_header_list = a_file.readline().split('\t')
+                a_header_list.pop()  # First item is useless; throw away
+                print("Allele header list:")
+                print(a_header_list)
+                m_header_list = m_file.readline().split('\t')
+                m_header_list.pop()  # Useless item; throw away
+                print("Metadata header list:")
+                print(m_header_list)
+                # while True:
+                #     try:
+                #         a_list = a_file.readline().split('\t')
+                #         a_name = a_list.pop(0)
+                #         m_list = m_file.readline().split('\t')
+                #         m_name = m_list.pop(0)
+                #         assert a_name == m_name
+                #         self.stdout.write(f"Importing sample {a_name}")
+                #         # Create document in MongoDB, popping off items of both a_list and m_list as they are needed
+                #     except EOFError as e:
+                #         print(e)  # Just to see which file threw the EOF
 
         # for line in the two files:
         #     # Or maybe through API?
