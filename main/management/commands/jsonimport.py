078faff4f925fac29ea6565eb4d37dbe0cb980cc
Finn Gruwier Larsen <figl@ssi.dk>
2022-11-25 09:11:24 +0100

    Let jsonimport create a dataset.

--- main/management/commands/jsonimport.py
+++ main/management/commands/jsonimport.py
@@ -4,6 +4,7 @@ import json
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
+from main.models import DataSet
 
 import pymongo
 
@@ -33,6 +34,7 @@ class Command(BaseCommand):
         
         p = folder.glob('*.json')
         files = [x for x in p if x.is_file()]
+        mongo_keys = list()  # org, name key pairs to add to dataset
 
         for file in files:
             self.stdout.write(f"Importing file {file.name}...")
@@ -43,10 +45,19 @@ class Command(BaseCommand):
                 result = db.samples.insert_one(content)
                 if result.acknowledged:
                     self.stdout.write(self.style.SUCCESS(f"Org {content['org']} name {content['name']} added to MongoDB."))
+                    mongo_keys.append({'org': content['org'], 'name': content['name']})
                 else:
                     self.stdout.write(self.style.ERROR(f"Could not update sample in MongoDB: org: {content['org']}, name {content['name']}"))
                 
 
         number = db.samples.count_documents({})
         self.stdout.write(f'After import MongoDB contains {str(number)} samples.')
+        self.stdout.write(f"Creating dataset {options['dataset']}.")
+        dataset = DataSet(
+            species=options['sp'],
+            name=options['dataset'],
+            description="Imported with jsonmport",
+            mongo_keys=mongo_keys
+            )
+        dataset.save()
         self.stdout.write(self.style.SUCCESS('Success!'))
