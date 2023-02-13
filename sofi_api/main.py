1bdf5a0b694795eb3e9c8a7e736c91cfcacae091
Finn Gruwier Larsen <figl@ssi.dk>
2023-02-13 11:48:45 +0100

    Create dataframe from Mongo cursor.

--- sofi_api/main.py
+++ sofi_api/main.py
@@ -3,6 +3,7 @@ from os import getenv
 
 from pydantic import BaseModel
 from fastapi import FastAPI
+import pandas
 
 from mongo import samples_api
 from partitioning_HC import HC
@@ -27,9 +28,12 @@ async def start_job(job: HCRequest):
     print(job.ids)
     mongo_cursor, unmatched = sapi.get_samples_from_keys(job.ids, fields={'name', 'allele_profile'})
     
-    for s in mongo_cursor:
-        print(s['name'])
-        print(s['allele_profile'][:10])
+    # for s in mongo_cursor:
+    #     print(s['name'])
+    #     print(s['allele_profile'][:10])
+
+    allele_profiles = pandas.DataFrame(mongo_cursor)
+    print(allele_profiles)
 
     return {
         "OK": "Thanks."
