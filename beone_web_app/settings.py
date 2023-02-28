a76f4397d078d4d24f84d5c7b55e56e48aa1faeb
Finn Gruwier Larsen <figl@ssi.dk>
2023-02-28 09:20:47 +0100

    More uncommenting.

--- beone_web_app/settings.py
+++ beone_web_app/settings.py
@@ -210,7 +210,7 @@ REPORTREE_JOB_FOLDER = Path('/rt_runs')  # Root is root of Docker container!
 
 REPORTREE_TIMEOUT = 5  # The number of seconds to wait for at REST response before setting job state to 'RUNNING'
 
-try:
-    from settings_local import *
-except ImportError:
-    pass
+# try:
+#     from settings_local import *
+# except ImportError:
+#     pass
