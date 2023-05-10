3235080e6872b4b45c998a1add505ad7ad561f38
Finn Gruwier Larsen <figl@ssi.dk>
2023-05-10 14:22:34 +0200

    Add admin classes

--- comparisons/admin.py
+++ comparisons/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from comparisons.models import Species, SequenceSet, BaseTool, Comparison, Cluster, PotentialOutbreak
+
+class SpeciesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Species, SpeciesAdmin)
+
+
+class SequenceSetAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(SequenceSet, SequenceSetAdmin)
+
+
+class BaseToolAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(BaseTool, BaseToolAdmin)
+
+
+class ComparisonAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Comparison, ComparisonAdmin)
+
+class ClusterAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Cluster, ClusterAdmin)
+
+
+class PotentialOutbreakAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PotentialOutbreak, PotentialOutbreakAdmin)
