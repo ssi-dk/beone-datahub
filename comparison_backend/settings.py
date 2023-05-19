4c647e8e4e8c549056bcfe961e42b29ff7cce578
Finn Gruwier Larsen <figl@ssi.dk>
2023-05-19 12:56:05 +0200

    Add settings.BIO_API_PORT

--- comparison_backend/settings.py
+++ comparison_backend/settings.py
@@ -212,7 +212,4 @@ REPORTREE_JOB_FOLDER = Path('/rt_runs')  # Root is root of Docker container!
 
 REPORTREE_TIMEOUT = 5  # The number of seconds to wait for at REST response before setting job state to 'RUNNING'
 
-# try:
-#     from settings_local import *
-# except ImportError:
-#     pass
+BIO_API_PORT = 8001
