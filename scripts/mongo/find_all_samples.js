a6347e8c0edcc5a8038f9032fdead60b42bb4817
Finn Gruwier Larsen <figl@ssi.dk>
2023-05-22 12:37:09 +0200

    Add script find_all_samples.js.

---
+++ scripts/mongo/find_all_samples.js
@@ -0,0 +1,6 @@
+// This script can be executed from the command line ind the mongo container by typing:
+// mongosh -f /scripts/find_sample_one.js
+
+config.set('inspectDepth', Infinity);
+db = connect( 'mongodb://localhost/bifrost_test' );
+printjson(db.samples.find({}));
