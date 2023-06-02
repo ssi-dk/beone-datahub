0cf9b3fa0c01c1bb04a7eb124dfab1ab59677fcb
Finn Gruwier Larsen <figl@ssi.dk>
2023-06-02 10:37:16 +0200

    More work.

--- comparisons/management/commands/fakesamples.py
+++ comparisons/management/commands/fakesamples.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from sys import exit
 import json
+import random
+import string
 from os import getcwd
 
 from django.core.management.base import BaseCommand, CommandError
@@ -8,6 +10,21 @@ from django.conf import settings
 from main.models import DataSet
 
 import pymongo
+
+def rndstr(length):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+def rndpct():
+    i: int = random.randint(0, 9999)
+    f: float = i / 100
+    s: str = '{:.2f}'.format(f)
+    return f
+
+def allele_generator():
+    locus = 31717
+    while locus <= 40274:
+        yield "INNUENDO_cgMLST-000" + str(locus), str(999)
+        locus += 1
 
 class Command(BaseCommand):
     help = "Generate fake samples with allele profiles in MongoDB."
@@ -21,8 +38,20 @@ class Command(BaseCommand):
         db = connection.get_database()
         number = db.samples.count_documents({})
         self.stdout.write(f'Currently MongoDB contains {str(number)} samples.')
+        run_name = rndstr(10)
+        self.stdout.write(f"Fake run name: {run_name}")
         template_file = Path(getcwd(), 'comparisons', 'management', 'commands', 'fakesample_template.json')
         template = open(template_file, 'r')
+        sample = json.load(template)
+
+        sample_name = rndstr(10)
+        self.stdout.write(f"Short sample name: {sample_name}")
+        sample['name'] = sample_name
+        sample['categories']['sample_info']['summary']['sample_name'] = run_name + '_' + sample_name
+        sample['categories']['cgmlst']['summary']['call_percent'] = rndpct()
+        for (locus, value) in allele_generator():
+            sample['categories']['cgmlst']['report']['data']['alleles'][locus] = str(random.randint(1, 1000))
+        
 
         # folder = Path(options['folder'])
         # if not folder.exists():
