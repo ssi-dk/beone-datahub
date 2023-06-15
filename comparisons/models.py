08c45890ef9a4c27c487e8084a3397ee83363211
Finn Gruwier Larsen <figl@ssi.dk>
2023-06-15 11:24:17 +0200

    Remove many fields from Comparison

--- comparisons/models.py
+++ comparisons/models.py
@@ -87,22 +87,8 @@ class Comparison(models.Model):
 
     class Meta:
         ordering = ['-pk']
-
-    created_by = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    sequence_set = models.ForeignKey(SequenceSet, models.PROTECT)
     data_fields = ArrayField(models.CharField(max_length=25), default=list)   # default=get_default_data_fields
     field_data = models.JSONField(blank=True, default=dict)
-    status = models.CharField(max_length=15, choices=STATUSES, default='NEW')
-    started_at = models.DateTimeField(blank=True, null=True)
-    ended_at = models.DateTimeField(blank=True, null=True)
-    elapsed_time = models.DateTimeField(blank=True, null=True)
-    error_msg = models.CharField(max_length=80, blank=True, null=True)
-    folder_path = models.CharField(max_length=100, blank=True, null=True)
-    newick = models.TextField(blank=True, null=True)
-    base_tool = models.ForeignKey(BaseTool, on_delete=models.PROTECT, null=True)
-    linkage_method = models.CharField(max_length=10, choices=LINKAGE_METHODS, default='SINGLE')
-    params = models.JSONField(blank=True, default=dict)
     microreact_project = models.CharField(max_length=20, blank=True, null=True)
 
 
