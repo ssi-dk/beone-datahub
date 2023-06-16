c7ac732ea9411182ce7289b8ac11514340369065
Finn Gruwier Larsen <figl@ssi.dk>
2023-06-16 11:51:10 +0200

    Remove old views and urls

--- comparison_backend/urls.py
+++ comparison_backend/urls.py
@@ -25,19 +25,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='comparisons/login.html', next_page='/comparisons'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login'), name='logout'),
     path('sample_list/', views.sample_list, name='sample_list'),
-    path('comparisons/', views.list_comparisons, name='list_comparisons'),  #  Tag udgangspunkt i denne
-    path('sequence_sets/<int:dataset_key>/', views.view_dataset, name='view_dataset'),
-    path('sequence_sets/<int:dataset_key>/edit/', views.edit_dataset, name='edit_dataset'),
-    path('datasets/add_remove_sample/', views.add_remove_sample),
-    path('dashboard/', views.sample_list, name='dashboard'),
-    path('rt_jobs/', views.rt_jobs, name='rt_jobs'),              #  Skal merges in i den anden
-    path('rt_jobs/for_dataset/<int:dataset_key>/', views.rt_jobs, name='rt_jobs_for_dataset'),
-    path('rt_jobs/<int:rt_job_key>/delete/', views.delete_rt_job, {'dataset_page': False}, name='delete_rt_job'),
-    path('rt_jobs/<int:rt_job_key>/delete/for_dataset/', views.delete_rt_job, {'dataset_page': True}, name='delete_rt_job_for_dataset'),
-    path('rt_jobs/<int:rt_job_key>/run/', views.run_rt_job, name='run_rt_job'),
-    path('rt_jobs/<int:rt_job_key>/', views.view_rt_job, name='view_rt_job'),
-    path('rt_jobs/<int:rt_job_key>/output/<str:item>/', views.view_rt_output, name='view_rt_output'),
-    path('rt_jobs/<int:rt_job_key>/download/<str:item>/', views.download_rt_file, name='download_rt_file'),
-    path('rt_jobs/<int:rt_job_key>/data/', views.get_rt_data),
-    path('rt_jobs/<int:rt_job_key>/partitions/', views.get_partitions_for_job),
+    path('comparisons/', views.list_comparisons, name='list_comparisons'),
 ]
