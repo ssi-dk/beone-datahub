1b906c52f430485a9db7655bc297ea282b81129b
Finn Gruwier Larsen <figl@ssi.dk>
2023-07-10 11:35:46 +0200

    Add  more fields to list_display

--- comparisons/admin.py
+++ comparisons/admin.py
@@ -23,18 +23,19 @@ admin.site.register(Tree, TreeAdmin)
 
 
 class ComparisonAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'species')
     readonly_fields = ('species', 'created_by', 'sequences', 'distances')
 
 admin.site.register(Comparison, ComparisonAdmin)
 
 
 class ClusterAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'st', 'cluster_number')
+    list_display = ('pk', 'species', 'st', 'cluster_number')
 
 admin.site.register(Cluster, ClusterAdmin)
 
 
 class PotentialOutbreakAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'created_by', 'created_at', 'cluster', 'suspected_source')
+    list_display = ('pk', 'created_by', 'created_at', 'cluster', 'suspected_source', 'outbreak_id')
 
 admin.site.register(PotentialOutbreak, PotentialOutbreakAdmin)
