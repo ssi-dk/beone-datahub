3f4726c9a577c2a2ec4626dc022f03d126be382d
Finn Gruwier Larsen <figl@ssi.dk>
2023-07-13 13:40:15 +0200

    Add an attempt to structure data using dataclasses

--- bio_api/classes.py
+++ bio_api/classes.py
@@ -1,30 +1,63 @@
 from dataclasses import dataclass
 from datetime import date
 
+from persistence.bifrost_sample_template import bifrost_sample_template
+
 """The purpose of these classes is to organize data in a logic and efficient way.
 They have automatically generated __init__() methods as documented here:
 https://docs.python.org/3/library/dataclasses.html#module-dataclasses
 """
 
 @dataclass
-class Organization:
-    name: str
+class CommonMetadata:
+    pass # add common metadata fields here. See both sap_lims_metadata and sap_tbr_metadata; remember there are both mandadory and optional fields
 
 @dataclass
-class SequencingRun:
-    run_name: str
-    run_date: date
+class SSIMetadata(CommonMetadata):
+    pass # add SSI-specific metadata fields here See sap_tbr_metadata; remember there are both mandadory and optional fields
+
+@dataclass
+class FVSTMetadata(CommonMetadata):
+    pass # add FVST-specific metadata fields here See sap_lims_metadata; remember there are both mandadory and optional fields
 
 @dataclass
 class Sample:
     sample_id: str
-    owner: Organization
-    run: SequencingRun
-    imported_metadata: dict  # {'tbr': ..., 'lims': ...}
+
+@dataclass
+class SSISample(Sample):
+    metadata: SSIMetadata
+
+@dataclass
+class FVSTSample(Sample):
+    metadata: FVSTMetadata
+
+@dataclass
+class AnalysisResults:
+    pass  # se bifrost_sample_template.py
+
+@dataclass
+class Assessments:
+    pass  # se sap_manual_metadata
 
 @dataclass
 class Sequence:
     sequence_id: str
-    sample: Sample
-    analysis_results: dict
-    assessments: dict
+    sample: SSISample or FVSTSample
+    analysis_results: dict or None
+    assessments: dict or None
+
+
+if __name__ == "__main__":
+    metadata = SSIMetadata()
+    sample = SSISample(sample_id='sample_id_1', metadata=metadata)
+    analysis_results = AnalysisResults()
+    assessments = Assessments()
+    sequence = Sequence(
+        sequence_id='sequence_id_1',
+        sample=sample,
+        analysis_results=analysis_results,
+        assessments=assessments
+        )
+    print(type(sequence))
+    print(sequence)
