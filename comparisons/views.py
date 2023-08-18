1ecb69a7e73ac53b7825ac342adb5f16f75871e0
Finn Gruwier Larsen <figl@ssi.dk>
2023-08-18 10:52:08 +0200

    Use newick instead og Tree object

--- comparisons/views.py
+++ comparisons/views.py
@@ -213,12 +213,13 @@ def launchpad(request, tree_id):
             # print(tree_encoded)
             # TODO Repeat The code section above for lims metadata and manual metadata
             project_name = request.user.username + '_' + str(timezone.now())
-            project = assemble_project(project_name, metadata_keys, metadata_values, tree)
+            project = assemble_project(project_name, metadata_keys, metadata_values, tree.newick)
             print(project.files[0].body)
+            print(project.files[1].body)
             print(project.to_dict())
             #TODO Access-Token must be saved per user
             access_token = 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..aoxg7jxHXGKS5gsU.9lcYdFeogzy9mEth0aAy3FmFucmDCAd0HVwnz5ssm3dKvY_jVkRc_UviOs0K8mimGzZBE4btSPpmh-B9rN7ba6x6Bt2aIjEuY526hxSUjzTrot6V4F0auVJOfHmtU4U106jAS2pD5kte4H51GfCRVw.35f_9LCrIg0lWpkO_Geekw'
-            
+
             # response = requests.post(
             #     f'{settings.MICROREACT_BASE_URL}/api/projects/create/',
             #     headers= {
