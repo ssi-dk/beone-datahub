53a022049df27797b9fa79afb5eccf60cfb9f960
Finn Gruwier Larsen <figl@ssi.dk>
2023-08-23 13:52:48 +0200

    Insert a comment

--- comparisons/views.py
+++ comparisons/views.py
@@ -188,6 +188,11 @@ def launchpad(request, tree_id):
             metadata_values = list()
             first_record = next(metadata)
             metadata_keys = list(first_record.keys())
+            print(metadata_keys)
+            # Der er et uhåndteret potentielt problem her. Koden går ud fra, at alle key/value par
+            # eksisterer på alle dokumenter, og det kan vi ikke være sikre på!
+            # Vi er nødt til for hvert eneste dokument at tjekke, om keys er de samme som dem, der
+            # er listet i tbr_data_fields, og hvis der mangler en key, skal der indsættes None
             metadata_values.append(list(first_record.values()))
             while True:
                 try:
